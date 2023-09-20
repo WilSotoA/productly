0f28eeef117808d3c26266bf2cd44a65981e3d82
WilSotoA <113364120+WilSotoA@users.noreply.github.com>
2023-09-20 15:16:14 -0500

    make-models-and-execute-migrations

--- productos/models.py
+++ productos/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=255)
+
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=255)
+    stock = models.IntegerField()
+    puntaje = models.FloatField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    creado_en = models.DateTimeField(default=timezone.now)
