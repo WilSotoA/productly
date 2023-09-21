20a02970488fc7792e965d3928a942ab8f26dcf5
WilSotoA <113364120+WilSotoA@users.noreply.github.com>
2023-09-21 17:09:22 -0500

    add-templates-for-products

--- productos/views.py
+++ productos/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from .models import Producto
 
 
 # Create your views here.
 def index(req):
-    return HttpResponse("Hola mundo!")
+    productos = Producto.objects.all().values()
+    return render(req, "index.html", context={"productos": productos})
