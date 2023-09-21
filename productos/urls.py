db3314d14ededee47ea7595bacfc0f8071f211d8
WilSotoA <113364120+WilSotoA@users.noreply.github.com>
2023-09-21 18:10:54 -0500

    add-form-for-create

--- productos/urls.py
+++ productos/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = "products"
+
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:id>", views.detail, name="detail"),
+    path("form", views.form, name="create"),
 ]
--- productos/views.py
+++ productos/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
+from django.http import Http404, HttpResponse, HttpResponseRedirect, JsonResponse
+from django.shortcuts import render, get_object_or_404
+from .forms import ProductoForm
 from .models import Producto
 
 
@@ -7,3 +8,19 @@ from .models import Producto
 def index(req):
     productos = Producto.objects.all().values()
     return render(req, "index.html", context={"productos": productos})
+
+
+def detail(req, id):
+    producto = get_object_or_404(Producto, id=id)
+    return render(req, "detail.html", context={"producto": producto})
+
+
+def form(req):
+    if req.method == "POST":
+        form = ProductoForm(req.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/products")
+    else:
+        form = ProductoForm()
+    return render(req, "form.html", {"form": form})
